aa6800e8c70d895174e2bccfd053c41a5ec4bd8d
songshu007 <74942051+songshu007@users.noreply.github.com>
2022-07-09 10:01:47 +0800

    Add files via upload

---
+++ example/Rectshape.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Drawable.h"
+
+/*矩形类*/
+class Rectshape : public Drawable
+{
+public:
+	Rectshape();
+	~Rectshape();
+	void setSize(const D2D1_POINT_2F& size);
+
+	const D2D1_POINT_2F& getSize();
+
+private:
+	virtual void draw(ID2D1HwndRenderTarget* RenderTarget);
+
+	D2D1_RECT_F m_Rect;			// 矩形
+	D2D1_POINT_2F m_Size;		// 大小
+};
+
